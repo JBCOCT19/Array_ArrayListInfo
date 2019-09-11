4b899b7400eb37809bc8c4b6f78905306b0b222a
jnfrle <jennifer.lee1@montgomerycollege.edu>
2019-09-11 11:44:59 -0400

    after class commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
-  public static void main(String[] args){
+    public static void main(String[] args) {
 
     /*  Limitations of Arrays
 
@@ -12,75 +12,75 @@ public class Main {
 
      */
 
-    // Note:  We may choose to use arrays over ArrayLists when we know the number of items
-    //        Using arrays is faster than ArrayLists when processing
+        // Note:  We may choose to use arrays over ArrayLists when we know the number of items
+        //        Using arrays is faster than ArrayLists when processing
 
-    /***************************************************************************************************
-     *   ARRAYS                                 |    ARRAYLISTS
-     * -------------------------------------------------------------------------------------------------
-     * Declaration:                             |
-     *    String[] colors = new String[3];      |   ArrayList<String> colors = new ArrayList<>();
-     * -------------------------------------------------------------------------------------------------
-     * Assigning values:                        | Adding values:
-     *    colors[0] = "blue";                   |   colors.add("blue");
-     * -------------------------------------------------------------------------------------------------
-     * Removing items:                          |
-     *    You cannot delete items from arrays   |   colors.remove("blue") or colors.remove(0)
-     * -------------------------------------------------------------------------------------------------
-     * Getting the number of items:             |
-     *    colors.length;                        |   colors.size();
-     * -------------------------------------------------------------------------------------------------
-     * Looping through and printing the values: | Enhanced for loop (can be used for arrays):
-     *    for(int i=0; i<colors.length; i++) {  |   for(String s : colors) {
-     *         System.out.println(colors[i]);   |       System.out.println(s);
-     *    }                                     |   }
-     ****************************************************************************************************/
+        /***************************************************************************************************
+         *   ARRAYS                                 |    ARRAYLISTS
+         * -------------------------------------------------------------------------------------------------
+         * Declaration:                             |
+         *    String [] colors = new String[3];      |   ArrayList<String> colors = new ArrayList<>();
+         * -------------------------------------------------------------------------------------------------
+         * Assigning values:                        | Adding values:
+         *    colors[0] = "blue";                   |   colors.add("blue");
+         * -------------------------------------------------------------------------------------------------
+         * Removing items:                          |
+         *    You cannot delete items from arrays   |   colors.remove("blue") or colors.remove(0)
+         * -------------------------------------------------------------------------------------------------
+         * Getting the number of items:             |
+         *    colors.length;                        |   colors.size();
+         * -------------------------------------------------------------------------------------------------
+         * Looping through and printing the values: | Enhanced for loop (can be used for arrays):
+         *    for(int i=0; i<colors.length; i++) {  |   for(int i=0; i<colors.size(); i++) {
+         *         System.out.println(colors[i]);   |       System.out.println(colors[i]);
+         *    }                                     |   }
+         ****************************************************************************************************/
 
 
-    // Declare an array list of words
-    ArrayList<String> words = new ArrayList<>();
+        // Declare an array list of words
+        ArrayList<String> words = new ArrayList<>();
 
-    // Add words to the array list
-    words.add("snowy");
-    words.add("cloudy");
-    words.add("rainy");
-    words.add("sunny");
-    words.add("foggy");
+        // Add words to the array list
+        words.add("snowy");
+        words.add("foggy");
+        words.add("rainy");
+        words.add("sunny");
+        words.add("snowy");
+        words.add("cloudy");
+        words.add("snowy");
 
-    words.add("snowy");
-    System.out.println(words);
+        System.out.println("Current list: " + words);
 
-//    words.remove("snowy"); // only removes first instance
+//    words.remove("snowy"); // only removes first occurrence
 //    System.out.println(words);
 
-    while (words.contains("snowy")){
-        words.remove("snowy");
-    }
-    System.out.println(words);
+        while (words.contains("snowy")) { // need loop to remove other ones
+            words.remove("snowy");
+        }
+        System.out.println("Current list: " + words);
 
-    // Check if list contains the word "snowy"
-    if(words.contains("foggy")){
-      System.out.println("Foggy found!\n");
-    }
+        // Check if list contains the word "foggy"
+        if (!words.contains("foggy")) {
+            System.out.println("Foggy found!\n");
+        }
 
-    // Check if list is empty
-    if(words.isEmpty()){
-      System.out.println("List empty!\n");
-    }
-    else {
-      System.out.println("List is not empty!\n");
-    }
+        // Check if list is empty
+        if (words.isEmpty()) {
+            System.out.println("List empty!\n");
+        } else {
+            System.out.println("List is not empty!\n");
+        }
 
-    // Get the word at index 2
-    System.out.println(words.get(2) + "\n");
+        // Get the word at index 2
+        System.out.println("Word at index 2" + words.get(2) + "\n");
 
-    // Sort the list of words alphabetically
-    Collections.sort(words);
+        // Sort the list of words alphabetically
+        Collections.sort(words); //
 
-    // Print out list after sorting
-    System.out.println("List after sorting:");
-    for(String s : words) {
-      System.out.println(s);
+        // Print out list after sorting
+        System.out.println("Current list (after sort):");
+        for (String s : words) {
+            System.out.println(s);
+        }
     }
-  }
 }
